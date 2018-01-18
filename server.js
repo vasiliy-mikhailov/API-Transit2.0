6b4cab2d419c2d16bc7c90ec4cdb73f8e6d8b02f
Vasiliy Mikhailov <mikhailov@emociono.com>
2018-01-18 17:16:09 +0300

    origin

--- server.js
+++ server.js
@@ -6,11 +6,27 @@ var loki = require('lokijs');
 var app = express();
 app.use(bodyParser.json());
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server, {origins:'*'});
+// Add headers
+app.use(function (req, res, next) {
 
-// Initialize the app.
-server.listen(process.env.PORT || 8080, function () {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
